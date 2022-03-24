cd3c8d28ac35a6aa0495efc2a42f8cc6985e4957
Darkempire <50015928+Darkempire78@users.noreply.github.com>
2022-03-24 23:06:07 +0100

    improve calculation

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-from flask import Flask, render_template, request, send_from_directory, current_app
 import os
-from concurrent.futures import ProcessPoolExecutor
+from flask import Flask, render_template, request, flash, Response
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24).hex()
+
+calculation = []
 
 @app.route("/")
 def home():
-    return(render_template("calculation.html"))
+    return(render_template("calculation.html", calculation=[]))
 
-@app.route('/submit', methods=['POST'])
+@app.route("/submit", methods=["GET", "POST"])
 def submit():
-    value = request.form['text']
+    value = request.form["text"]
     print(value)
-    return value
+    calculation.append((value, "result"))
+    return(render_template("calculation.html", calculation=calculation))
+
+
+# https://towardsdatascience.com/using-python-flask-and-ajax-to-pass-information-between-the-client-and-server-90670c64d688
+# https://www.digitalocean.com/community/tutorials/how-to-use-web-forms-in-a-flask-application
 
 if __name__ == "__main__":
     app.run(debug=True)
