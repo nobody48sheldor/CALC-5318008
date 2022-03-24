6de990a249bf9b85b5f400644e1da0b0047f9c1b
Darkempire <50015928+Darkempire78@users.noreply.github.com>
2022-03-24 21:54:11 +0100

    add calculation.html

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return(render_template("main.html"))
+    return(render_template("calculation.html"))
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    value = request.form['text']
+    print(value)
+    return value
 
 if __name__ == "__main__":
     app.run(debug=True)
