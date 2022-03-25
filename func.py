0a0d3ab8701877d0ccf343defa9d6a0d2750c335
nobody48sheldor <lolmyizi@gmail.com>
2022-03-25 22:08:53 +0100

    update

--- func.py
+++ func.py
@@ -1,19 +1,27 @@
 import numpy as np
-#from math import *
+from math import *
 from cmath import *
+from sympy import *
+
+x, y, z = symbols('x y z')
 
 def calculate(input):
     input = input.replace("^", "**")
+    input = input.replace("I", "integrate")
+    input = input.replace("D", "diff")
+    input = input.replace("L", "limit")
+    input = input.replace("E", "expr.series")
+    input = input.replace("S", f"solveset(Eq{input[1:]})")
+    print(input)
     try:
         result = eval(input)
         print(type(result))
         if isinstance(result,complex):
-            print("here")
-            print(result.real(), result.imag())
-            if result.imag() == 0.0:
-                result = result.real()
-            if result.real() == 0.0:
-                result = result.imag()*1j
+            print(result.real, result.imag)
+            if result.imag == 0:
+                result = result.real
+            if result.real == 0:
+                result = result.imag*1j
+        return(str(result))
     except Exception as e:
         return(e)
-    return(result)
