c3a305b2bdebc74efe3f4b149a0fedfca0896d3c
nobody48sheldor <lolmyizi@gmail.com>
2022-03-25 23:05:59 +0100

    equations

--- func.py
+++ func.py
@@ -7,14 +7,22 @@ x, y, z = symbols('x y z')
 
 def calculate(input):
     input = input.replace("^", "**")
+    input = input.replace("=", ",")
+    input = input.replace("oo", "inf")
     input = input.replace("I", "integrate")
     input = input.replace("D", "diff")
     input = input.replace("L", "limit")
     input = input.replace("E", "expr.series")
-    input = input.replace("S", f"solveset(Eq{input[1:]})")
+    input = input.replace("S", "solveset(Eq")
+    print(input[0])
+    if input[0] == "s":
+        input = input + ")"
+        print(input)
     try:
         result = eval(input)
+        print(type(result))
         if isinstance(result,complex):
+            print(result.real, result.imag)
             if result.imag == 0:
                 result = result.real
             if result.real == 0:
