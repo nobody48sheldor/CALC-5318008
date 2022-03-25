2c40bf090eec0c976d4de56f52652ce8ffc633b8
nobody48sheldor <lolmyizi@gmail.com>
2022-03-25 20:33:06 +0100

    push

--- func.py
+++ func.py
@@ -1,11 +1,19 @@
 import numpy as np
-from math import *
+#from math import *
 from cmath import *
 
 def calculate(input):
     input = input.replace("^", "**")
     try:
         result = eval(input)
+        print(type(result))
+        if isinstance(result,complex):
+            print("here")
+            print(result.real(), result.imag())
+            if result.imag() == 0.0:
+                result = result.real()
+            if result.real() == 0.0:
+                result = result.imag()*1j
     except Exception as e:
         return(e)
     return(result)
