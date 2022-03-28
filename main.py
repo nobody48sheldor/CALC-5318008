c234c908b1730463de251dcd34b70d7086990179
Darkempire <50015928+Darkempire78@users.noreply.github.com>
2022-03-28 21:06:56 +0200

    add config.json

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import os
 import func
+import json
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24).hex()
 
+with open("config.json", "r") as file:
+    config = json.load(file)
+
 calculation = []
 
 @app.route("/")
 def home():
-    return(render_template("main.html", calculation=[]))
+    return(render_template("main.html", calculation=[], config=config))
 
 @app.route("/", methods=["GET", "POST"])
 def redirectCalc():
     value = request.form["menu"]
     if value == "calc":
-        return(render_template("calculation.html"))
+        return(render_template("calculation.html", calculation=[], config=config))
     if value == "function":
-        return(render_template("main.html"))
+        return(render_template("main.html", config=config))
     if value == "program":
-        return(render_template("main.html"))
+        return(render_template("main.html", config=config))
     else:
-        return(render_template("main.html"))
+        return(render_template("main.html", config=config))
 
 
 @app.route("/submit", methods=["GET", "POST"])
@@ -29,7 +33,7 @@ def submit():
     value = request.form["text"]
     result = str(func.calculate(value))
     calculation.insert(0, (value, result))
-    return(render_template("calculation.html", calculation=calculation))
+    return(render_template("calculation.html", calculation=calculation, config=config))
 
 # https://towardsdatascience.com/using-python-flask-and-ajax-to-pass-information-between-the-client-and-server-90670c64d688
 # https://www.digitalocean.com/community/tutorials/how-to-use-web-forms-in-a-flask-application
